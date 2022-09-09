00f532735ec0c21f5b11f9cb818a1258dfad7a71
Muhammed Ali SUSLU <malisuslu@yandex.com>
2022-09-09 06:07:52 +0300

    Delete model configuered, some small correction applied on validation alerts upon update, form title configuered.

--- student_register/urls.py
+++ student_register/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import student_add_update, student_delete, student_list, verify_delete
+from .views import student_add_update, student_delete, student_list
 
 urlpatterns = [
-    path('', student_add_update, name='form'),
+    path('', student_add_update.as_view(), name='form'),
     path('student_list/', student_list, name='list'),
-    path('update/<int:id>/', student_add_update, name='update'),
-    path('verifydelete/<int:id>/', verify_delete, name='verifydelete'),
+    path('update/<int:id>/', student_add_update.as_view(), name='update'),
     path('delete/<int:id>/', student_delete, name='delete'),
 ]
--- student_register/views.py
+++ student_register/views.py
@@ -1,22 +1,68 @@
+from re import fullmatch
 from django.shortcuts import render, redirect
 from .models import Student
 from .forms import StudentForm
 from django.contrib import messages
+from django.views import View
 
 # Create your views here.
 
+# def student_add_update(request, id=0):
+#     if request.method == "POST":
+#         if id == 0:
+#             form = StudentForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 messages.success(request, "Registration Successfull ✓✓")
+#                 form = StudentForm()
+#             else:
+#                 messages.error(request, "Registration failed!!")
+#                 form = StudentForm(request.POST)
+#         else:
+#             student = Student.objects.get(pk=id)
+#             form = StudentForm(request.POST, instance=student)
+#             if form.is_valid():
+#                 form.save()
+#                 messages.success(request, "Updated Successfully ✓✓")
+#                 form = StudentForm()
+#             else:
+#                 messages.error(request, "Update failed!!")
+#                 form = StudentForm(request.POST)
 
-def student_add_update(request, id=0):
-    if request.method == "POST":
+#     else:
+#         if id==0:
+#             form = StudentForm()
+#         else:
+#             student = Student.objects.get(pk=id)
+#             form = StudentForm(instance=student)
+
+#     context = {
+#         "form" : form
+#     }
+#     return render(request, 'student_register/student_form.html', context)
+
+
+
+#######   Class View Method   ##########
+class student_add_update(View):
+    def get(self, request, id=0):
+        if id == 0:
+            form = StudentForm()
+        else:
+            student = Student.objects.get(pk=id)
+            form = StudentForm(instance=student)
+        return render(request, 'student_register/student_form.html', {"form": form})
+
+    def post(self, request, id=0):
         if id == 0:
             form = StudentForm(request.POST)
             if form.is_valid():
                 form.save()
-                messages.success(request, "Registration Successfull ✓✓")
+                messages.success(request, "Registaration Successfull ✓✓")
                 form = StudentForm()
             else:
-                messages.error(request, "Registration failed!!")
-                form = StudentForm(request.POST)    
+                messages.error(request, "Registaration failed!!")
+                form = StudentForm(request.POST)
         else:
             student = Student.objects.get(pk=id)
             form = StudentForm(request.POST, instance=student)
@@ -28,32 +74,14 @@ def student_add_update(request, id=0):
                 messages.error(request, "Update failed!!")
                 form = StudentForm(request.POST)
 
-    else:
-        if id==0:
-            form = StudentForm()
-        else:
-            student = Student.objects.get(pk=id)
-            form = StudentForm(instance=student)
-
-    context = {
-        "form" : form
-    }
-    return render(request, 'student_register/student_form.html', context)
-
+        return render(request, 'student_register/student_form.html', {"form": form})
 
 
 def student_list(request):
     student_list = Student.objects.all()
-    return render(request, 'student_register/student_list.html', {"student_list" : student_list})
-
-def verify_delete(request, id):
-    student = Student.objects.get(pk=id)
-    return render(request, 'student_register/verify_delete.html', {"student": student})
+    return render(request, 'student_register/student_list.html', {"student_list": student_list})
 
 def student_delete(request, id):
     student = Student.objects.get(id=id)
     student.delete()
     return redirect('list')
-
-
-

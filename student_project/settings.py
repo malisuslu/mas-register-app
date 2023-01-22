17c03553c292a072b43492cdaaa0d07f55295e12
Muhammed Ali SUSLU <malisuslu@yandex.com>
2023-01-22 02:57:00 +0000

    PostgreSQL settings are arranged and the database is created.

--- student_project/settings.py
+++ student_project/settings.py
@@ -106,8 +106,13 @@ WSGI_APPLICATION = 'student_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'U0fbhmGGmj3ca7iJpod8',
+        'HOST': 'containers-us-west-184.railway.app',
+        'PORT': '7684',
     }
 }
 
